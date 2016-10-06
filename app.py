0959ec2654d48875a88168ae8a37b809122041d0
samw7823 <samw7823@users.noreply.github.com>
2016-10-06 18:33:45 -0400

    Updating app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
-app.config['DEBUG'] = FALSE #only true while testing
+app.config['DEBUG'] = True #only true while testing
 @app.route("/")
 def hello():
     return render_template('index.html')
